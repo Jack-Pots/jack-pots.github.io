a43cc8d408959aeb6deb9c17dab18ce0e9f79c04
JPAUL <142168122+Jack-Pots@users.noreply.github.com>
2025-01-05 13:58:42 +0000

    Add Header To The Main Page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,11 +14,11 @@
   
 <div class="relative flex flex-col max-w-[1440px mx-auto w-full text-sm sm:text-base min-h-screen]">
     <div class={"fixed bottom-0 w-full duration-200 flex p-10 z-[-10] " + y > 0 ? 'opacity-full pointer-events-auto' : 'pointer-events-none opacity-0'}>
-        <!-- <button on:click={goTop} class="ml-auto rounded-full">
-            <i class="fa-solid fa-arrow-up" /> 
-        </button> --> <!--TODO: Issues with button-->
+        <button on:click={goTop} class="ml-auto rounded-full bg-slate-900 text-violet-400 px-3 sm:px hover:bg-slate-800 cursor-pointer">
+            <i class="fa-solid fa-arrow-up grid place-items-center aspect-square"/> 
+        </button>
     </div>
-    <Header />
+    <Header y={y}/>
     <slot />
     <Footer />
 </div>
