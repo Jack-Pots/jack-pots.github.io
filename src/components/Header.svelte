3d288594aaf2ea5a05e227025ca6c68baefa6601
JPAUL <142168122+Jack-Pots@users.noreply.github.com>
2025-01-06 06:44:29 +0000

    Fix Deployment Issues Caused By a Link Element [V.3]

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,8 +2,8 @@
     export let y; 
     
     let tabs = [
-        {name: "Projects", link: "#project"},
-        {name: "About Me", link: "#about"}
+        {name: "Projects", link: "#"},
+        {name: "About Me", link: "#"}
     ];    
 </script>
 
