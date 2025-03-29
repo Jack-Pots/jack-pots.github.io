faf87d17c6eaf8b74586e72067a9042f852714d0
JPAUL <142168122+Jack-Pots@users.noreply.github.com>
2025-03-29 09:44:28 -0700

    Add Location To The Main Page

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -13,6 +13,7 @@
             <div class="main">
                 <div class="content">
                     <p class="about">Hey, I am Joel Paul, an aspirant SDE also a passionate open sourcer developer! </p>
+                    <h6 class="where"><i class="fa-solid fa-map-pin"></i>  Bangalore, Karnataka, India</h6>
                     <p>As a Computer Science student, I began contributing to open-source projects in 2023 to enhance my practical skills and deepen my understanding of real-world software development.</p>
                 </div>
             </div>
