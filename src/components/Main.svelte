b76781eb4a6fd04d1b9911211dfe99923c22f95d
JPAUL <142168122+Jack-Pots@users.noreply.github.com>
2025-05-24 04:35:28 +0000

    Add Button to Connect Through Email Client. TODO: Dead Link

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -15,6 +15,11 @@
                 <div class="content">
                     <p class="about">Hey, I am Joel Paul, an aspirant SDE also a passionate open sourcer developer! </p>
                     <p>As a Computer Science student, I began contributing to open-source projects in 2023 to enhance my practical skills and deepen my understanding of real-world software development.</p>
+                    <div class="mail">
+                        <a href="#" class="btn">Let's Connect<span><ion-icon name="arrow-forward-outline"></ion-icon></span></a>
+                        <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+                        <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+                    </div>
                 </div>
             </div>
         </div>
