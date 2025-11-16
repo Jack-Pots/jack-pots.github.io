fbb2539261a600871bfa7f3adf6431218c2058cb
Jack-Pots <jpp3062005@gmail.com>
2025-11-16 21:47:28 +0530

    Fix Issues With h1 Styling

--- src/routes/blogs/+page.svelte
+++ src/routes/blogs/+page.svelte
@@ -1,15 +1,17 @@
 <script>
-    import Main from "../../components/Main.svelte"
-    import Footer from "../../components/Footer.svelte";
+  import Main from "../../components/Main.svelte";
+  import Footer from "../../components/Footer.svelte";
 </script>
 
 <div class="flex justify-center h-screen">
-    <div class="w-11/12 justify-center h-screen inline-block">
-        <div class="text-center m-10">
-            <h1>BLOGS [WIP]</h1>
-        </div>
-        <div class="bg-gred-900 w-1/2 h-28 m-auto rounded-2xl ">
-        
-        </div>
+  <div class="w-11/12 justify-center h-screen inline-block">
+    <div class="text-center m-10">
+      <h1 class="mt-24">BLOGS</h1>
     </div>
+    <div
+      class="bg-gred-900 flex w-1/2 h-28 m-auto rounded-2xl items-center justify-center">
+      <!--Insert Contents-->
+      <h1>WIP</h1>
+    </div>
+  </div>
 </div>
